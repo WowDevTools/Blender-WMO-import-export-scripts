fe619696518aa1ff1d60464860fa35a2705fa80e
skarnproject <skarnproject@gmail.com>
2017-03-03 20:28:01 +0300

    perform delete loose on export to keep meshes clean and prevent blender and game crashes

--- io_scene_wmo/wmo_group.py
+++ io_scene_wmo/wmo_group.py
@@ -614,6 +614,7 @@ class WMO_group_file:
             # triangualate mesh
             bpy.ops.object.mode_set(mode='EDIT')
             bpy.ops.mesh.select_all(action='SELECT')
+            bpy.ops.mesh.delete_loose()
             bpy.ops.mesh.quads_convert_to_tris()
             bpy.ops.mesh.select_all(action='DESELECT')
             
