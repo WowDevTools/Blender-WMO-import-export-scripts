3786796b071b0fce17f8a90ea1dcd58f2979d50a
skarnproject <skarnproject@gmail.com>
2017-06-20 15:10:48 +0300

    Force ambient flag for multiple MOCV models.

--- io_scene_wmo/wmo/wmo_group.py
+++ io_scene_wmo/wmo/wmo_group.py
@@ -879,7 +879,8 @@ class WMOGroupFile:
         if obj.WowVertexInfo.Blendmap != "":
             vg_blendmap = obj.vertex_groups.get(obj.WowVertexInfo.Blendmap)
             self.mogp.Flags |= MOGP_FLAG.HasTwoMOCV
-
+            self.root.Flags |= 0x1
+            
         if obj.WowVertexInfo.SecondUV != "":
             uv_second_uv = obj.data.uv_textures.get(obj.WowVertexInfo.SecondUV)
             self.mogp.Flags |= MOGP_FLAG.HasTwoMOTV
